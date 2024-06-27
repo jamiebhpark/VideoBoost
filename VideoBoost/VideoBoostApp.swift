164b28ef7d3cf103d27c840ed6462b40c5256cf7
JAMIEPARK90 <jamiebhpark@gmail.com>
2024-06-27 15:38:43 +0900

    Initial Commit

---
+++ VideoBoost/VideoBoostApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoBoostApp.swift
+//  VideoBoost
+//
+//  Created by JONGHUN PARK on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoBoostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
